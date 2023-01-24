a314b182ba492b1cf83c6a734bd34bed52897bfd
ayoub <ayoubousbia02@gmail.com>
2023-01-24 19:26:09 +0100

    add product and update crud methods

--- api-design-master/src/router.ts
+++ api-design-master/src/router.ts
@@ -1,32 +1,55 @@
 import { Router } from 'express'
+import { body, validationResult, oneOf} from 'express-validator'
+import { handleInputErrors } from './modules/middleware'
+import { getOneProduct, getProducts, createProduct, deleteProduct } from './handlers/product'
+import { getOneUpdate, getUpdates, createUpdate, updateUpdate } from './handlers/update'
 
 const router = Router()
 
 // Product
-router.get('/product', (req, res) => {
-    res.json({message: 'hello'})
+router.get('/product', getProducts)
+router.get('/product/:id', getOneProduct)
+router.put('/product/:id',body('name').isString(), handleInputErrors, (req, res) => {
+
 })
-router.get('/product/:id', () => {} )
-router.put('/product/:id', () => {} )
-router.post('/product', () => {} )
-router.delete('/product/:id', () => {} )
+router.post('/product', body('name').isString(), handleInputErrors, createProduct, (req, res) => {
+
+})
+router.delete('/product/:id', deleteProduct)
 
 
 // Update
 
-router.get('/update', () => {} )
-router.get('/update/:id', () => {} )
-router.put('/update/:id', () => {} )
-router.post('/update', () => {} )
-router.delete('/update/:id', () => {} )
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
+router.put('/update/:id', 
+   body('title').optional(), 
+   body('body').optional(),
+   body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+   body('version').optional(), updateUpdate)
+
+router.post('/update', 
+   body('title').exists().isString(),
+   body('body').exists().isString(),
+   createUpdate,
+)
+router.delete('/update/:id', deleteUpdate)
 
 
 //Update Point
-
 router.get('/updatepoint', () => {} )
 router.get('/updatepoint/:id', () => {} )
-router.put('/updatepoint/:id', () => {} )
-router.post('/updatepoint', () => {} )
+router.put('/updatepoint/:id', 
+           body('name').optional().isString(), 
+           body('description').optional().isString(),
+           () => {} 
+)
+router.post('/updatepoint', 
+           body('name').isString(), 
+           body('description').isString(),
+           body('updateId').exists().isString(),
+            () => {} 
+)
 router.delete('/updatepoint/:id', () => {} )
 
 
